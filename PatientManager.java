083dc9e9546fbc56ad6498b76db1363c4a6467e6
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-08-18 22:45:01 +0200

    Fix syntax errors and improve patient registration setup in PatientManager class

--- PatientManager.java
+++ PatientManager.java
@@ -1,3 +1,20 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
 public class PatientManager {
-    
+    public static void main(String[] args) {
+
+        //variable declaration
+        String patientID; String firstName; String LastName; String gender; String medicalCondition;
+        int age;
+        enum  Category {Inpatient, Outpatient, Emergency};
+
+
+        // Registration of a patient
+        Scanner scanner = new Scanner(System.in);
+
+        out.println("1. Registration of a patient:");
+        out.printn()
+        
+}
 }
