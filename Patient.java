dd04bc8b60e2ae45cfcc0c3fed553f098a3999d3
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-08-20 10:58:57 +0200

    Add constructor and getter/setter methods to Patient class

--- Patient.java
+++ Patient.java
@@ -6,4 +6,59 @@ public class Patient {
     private String medicalCondition;
     private String category;
 }
+ public Patient(String patientID, String firstName, String lastName, int age, String medicalCondition, String category) {
+        this.patientID = patientID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.medicalCondition = medicalCondition;
+        this.category = category;
+    }
 
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMedicalCondition() {
+        return medicalCondition;
+    }
+
+    public void setMedicalCondition(String medicalCondition) {
+        this.medicalCondition = medicalCondition;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
