83f796c8d5f935163f6fa618fa04d2a586e28b98
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-08-20 10:57:01 +0200

    Add properties to Patient class for patient details

--- Patient.java
+++ Patient.java
@@ -1,3 +1,9 @@
 public class Patient {
-
+    private String patientID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String medicalCondition;
+    private String category;
 }
+
