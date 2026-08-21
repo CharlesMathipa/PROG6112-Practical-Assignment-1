02094f02b764b62325f97c70a04a40f184453a54
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-08-22 00:36:33 +0200

    Encapsulate wardNumber and bedNumber fields in Inpatient class; add getter and setter methods

--- Inpatient.java
+++ Inpatient.java
@@ -1,11 +1,27 @@
 public class Inpatient extends Patient {
 
-    int wardNumber;
-    String bedNumber;
+    private int wardNumber;
+    private String bedNumber;
 
     public Inpatient(int wardNumber, String bedNumber, String patientID, String firstName, String lastName, int age, String gender, String medicalCondition) {
         super(patientID, firstName, lastName, age, gender, medicalCondition);
         this.wardNumber = wardNumber;
+        this.bedNumber = bedNumber;
+    }
+
+    public int getWardNumber() {
+        return wardNumber;
+    }
+
+    public void setWardNumber(int wardNumber) {
+        this.wardNumber = wardNumber;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
         this.bedNumber = bedNumber;
     }
 
